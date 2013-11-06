d66e232907372b26ffddc9a8995e5ff2b97ffcb4
rolfvdforst <rolf.v.d.forst@gmail.com>
2013-11-06 21:55:02 +0100

    Updated YAML v4.1.2 (Build: 130728)

--- css/yaml/core/js/yaml-focusfix.js
+++ css/yaml/core/js/yaml-focusfix.js
@@ -8,14 +8,12 @@
  *					http://www.communis.co.uk/blog/2009-06-02-skip-links-chrome-safari-and-added-wai-aria
  *                  Many thanks to Mathias Schäfer (http://molily.de/) for his code improvements
  *
- * @copyright       Copyright 2005-2012, Dirk Jesse
+ * @copyright       © 2005-2013, Dirk Jesse
  * @license         CC-BY 2.0 (http://creativecommons.org/licenses/by/2.0/),
  *                  YAML-CDL (http://www.yaml.de/license.html)
  * @link            http://www.yaml.de
  * @package         yaml
  * @version         4.0+
- * @revision        $Revision: 1.1 $
- * @lastmodified    $Date: 2012/09/19 15:45:04 $
  */
 
 (function () {
